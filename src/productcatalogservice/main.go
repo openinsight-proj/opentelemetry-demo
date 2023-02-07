52e10884e4dfafc9db0a4ee4d171175954b6716c
Jared Tan <jian.tan@daocloud.io>
2023-02-07 12:06:09 +0800

    revert
    Signed-off-by: Jared Tan <jian.tan@daocloud.io>

--- src/productcatalogservice/main.go
+++ src/productcatalogservice/main.go
@@ -23,7 +23,7 @@ import (
 	"os"
 	"strings"
 
-	pb "github.com/open-telemetry/opentelemetry-demo/src/productcatalogservice/genproto/hipstershop"
+	pb "github.com/opentelemetry/opentelemetry-demo/src/productcatalogservice/genproto/hipstershop"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 
 	"github.com/sirupsen/logrus"
